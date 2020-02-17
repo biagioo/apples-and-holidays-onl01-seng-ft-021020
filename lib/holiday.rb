94eddd1036266b9f72f83c74ed40259e8c6681f1
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-17 16:32:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,10 +77,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.collect do |season, holiday| 
-  holiday.collect do |x, y|
+holiday_hash.collect do |season, holidays| 
+  holidays.collect do |holiday, items|
+    items.include?("BBQ")
   
-  binding.pry
+  #binding.pry
     end
   end
 end
