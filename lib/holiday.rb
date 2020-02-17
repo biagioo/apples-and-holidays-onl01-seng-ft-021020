4898b964f76583d084572b8a72328e616c18ce69
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-17 15:43:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated has 
-  holiday_hash[:fall] = {:columbus_day => ["#{supply_array.chomp}"]}
+  holiday_hash[:fall] = {:columbus_day => ["#{supply_array}"]}
   
 end
 
