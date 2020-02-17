e146714bbc33888a08dc0cbdfc9dac8479f902ce
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-17 16:28:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays| 
   puts "#{season.capitalize}:" 
     holidays.each do |holiday, item|
-      puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+      puts "  #{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
     #binding.pry
       
     end
