b83b851c4f18ca219a0552ce5f88d744b17d1275
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-17 16:06:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season| 
-    binding.pry
+    #binding.pry
     if seasos == :winter 
       season.values.flatten 
      end
